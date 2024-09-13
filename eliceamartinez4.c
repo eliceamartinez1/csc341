a6dd8a1c64f0ad36fc9b21c2962865401f4df641
eddie <eliceamartinez1@toromail.csudh.edu>
2024-09-12 17:10:49 -0700

    Lab 4 program uploaded for lab 5, changes made to program

--- eliceamartinez4.c
+++ eliceamartinez4.c
@@ -5,24 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void stack()
-{
-	int a = 20;
-	printf("The address of the stack variable is %p\n", &a);
-}
+int x;
+int y = 19;
 
 
 int main(int argc)
 {	
-	int x;
-	int y = 4;
-	int *pointer1 = &argc;
-	int *values = (int *)malloc(sizeof(int)*5);
-	*values = 75;	
-	
-	printf("The address of the argc is %p\n", &pointer1);
-	stack();
-	printf("The address for heap variable is %p\n", &values);
+	int a;
+	int b = 0;
+	int *values;
+
+	values = (int *)malloc(sizeof(int)*5);
+
+	printf("The address of the argc is %p\n", &argc);
+	printf("The address of the stack variable is %p\n", &a);
+	printf("The address for heap variable is %p\n", values);
 	printf("The address for uninitialized data is %p\n", &x);
 	printf("The address for initialized data is %p\n", &y);
 
